52a5f9c08be9b7183a3d85520686d6dbe8a66754
Carter <djcart2010@gmail.com>
2026-01-26 19:12:02 -0700

    Kaia! I have added the RGB Color stuff here in "GoBildaRGB Test"

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBildaRGBTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "GoBILDA RGB Test")
+public class GoBildaRGBTest extends LinearOpMode {
+
+    Servo RGB;
+
+    @Override
+    public void runOpMode() {
+
+        // Name must match the Servo name in Robot Configuration
+        RGB = hardwareMap.get(Servo.class, "RGB");
+
+        waitForStart();
+
+
+        RGB.setPosition(0.65);
+
+
+        while (opModeIsActive()) {
+            // LED stays on, nothing else needed
+        }
+    }
+}
+
+
